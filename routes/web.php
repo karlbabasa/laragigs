2afc6d6af544534e7ed86452a1ec78f19882616d
Karl <karl.babasa@gmail.com>
2023-09-15 23:34:00 +0800

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+//ALL LISTINGS
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        //'heading' => 'Latest Listings',
+        'listings' => Listing::all()
+    ]);
 });
+
+//SINGLE LISTINGS
+Route::get('/listings/{id}', function($id) {
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
+});
+
+
+// Route::get('hello', function() {
+//     return response('<h1>hello world<h1>', 200)
+//     ->header('Content-Type', 'text/plain')
+//     ->header('foo', 'bar');
+// });
+
+// Route::get('/posts/{id}', function($id){
+//     //dd($id);
+//     return response('Post ' . $id);
+// })->where('id', '[0-9]+');
+
+// Route::get('/search', function(Request $request) {
+//     return $request->name . ' ' . $request->city;
+// });
